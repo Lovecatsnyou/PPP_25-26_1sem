75cf399612771ff7b6961789817b9ce2b00f2c61
Lovecatsnyou <witcherfromblair@gmail.com>
2025-10-17 14:25:53 +0300

    main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,11 @@
-
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+import numpy as np
+N = 10  
+num_shuffles = 1000 
+positions = np.zeros((N, N), dtype=int)
+for _ in range(num_shuffles):
+    arr = np.arange(1, N+1)
+    np.random.shuffle(arr)
+    for pos, number in enumerate(arr):
+        positions[pos, number-1] += 1
+for pos in range(N):
+    print(f'Позиция {pos+1}:', positions[pos])
